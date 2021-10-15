defaa2418b789b26cd78f65f879a701f3c228832
Brian Simpson <bsimps3@gmail.com>
2021-10-15 10:02:47 -0500

    basic movement implemented

--- main.ts
+++ main.ts
@@ -1,67 +1,58 @@
 input.onButtonPressed(Button.A, function () {
-    if (sprite.get(LedSpriteProperty.Direction) == 0 || sprite.get(LedSpriteProperty.Direction) == 180) {
-        sprite.set(LedSpriteProperty.Direction, -90)
-    }
+    direction = "left"
 })
 input.onPinPressed(TouchPin.P2, function () {
-    if (sprite.get(LedSpriteProperty.Direction) == -90 || sprite.get(LedSpriteProperty.Direction) == 90) {
-        sprite.set(LedSpriteProperty.Direction, 180)
-    }
+    direction = "down"
 })
 input.onButtonPressed(Button.B, function () {
-    if (sprite.get(LedSpriteProperty.Direction) == 0 || sprite.get(LedSpriteProperty.Direction) == 180) {
-        sprite.set(LedSpriteProperty.Direction, 90)
-    }
+    direction = "right"
 })
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
-    if (sprite.get(LedSpriteProperty.Direction) == -90 || sprite.get(LedSpriteProperty.Direction) == 90) {
-        sprite.set(LedSpriteProperty.Direction, 0)
-    }
+    direction = "up"
 })
-let sprite3: game.LedSprite = null
-let sprite2: game.LedSprite = null
+function move2nd () {
+	
+}
 let old_y = 0
 let old_y_2 = 0
 let old_x = 0
 let old_x_2 = 0
-let sprite: game.LedSprite = null
-basic.showLeds(`
-    . . . . .
-    . # # # .
-    . # . # .
-    . # # # .
-    . . . . .
-    `)
-game.setScore(1)
-sprite = game.createSprite(0, 0)
-let target = game.createSprite(randint(0, 4), randint(0, 4))
-while (true) {
+let direction = ""
+let score = 1
+direction = "right"
+let x = 0
+let y = 0
+led.toggle(x, y)
+let x_target = randint(0, 4)
+let y_target = randint(0, 4)
+led.toggle(x_target, y_target)
+while (x < 5 && (x >= 0 && (y < 5 && y >= 0))) {
     old_x_2 = old_x
     old_y_2 = old_y
-    old_x = sprite.get(LedSpriteProperty.X)
-    old_y = sprite.get(LedSpriteProperty.Y)
-    sprite.move(1)
-    if (sprite.isTouching(target)) {
-        game.addScore(1)
-        target.set(LedSpriteProperty.X, randint(0, 4))
-        target.set(LedSpriteProperty.Y, randint(0, 4))
-        if (game.score() == 2) {
-            sprite2 = game.createSprite(old_x, old_y)
-        }
-        if (game.score() == 3) {
-            old_x_2 = old_x
-            old_y_2 = old_y
-            sprite3 = game.createSprite(old_x_2, old_y_2)
-        }
+    old_x = x
+    old_y = y
+    // movement if statements
+    // 
+    if (direction == "right") {
+        led.toggle(x, y)
+        x += 1
+        led.toggle(x, y)
+    } else if (direction == "down") {
+        led.toggle(x, y)
+        y += 1
+        led.toggle(x, y)
+    } else if (direction == "left") {
+        led.toggle(x, y)
+        x += -1
+        led.toggle(x, y)
+    } else {
+        led.toggle(x, y)
+        y += -1
+        led.toggle(x, y)
     }
-    if (game.score() >= 2) {
-        sprite2.set(LedSpriteProperty.X, old_x)
-        sprite2.set(LedSpriteProperty.Y, old_y)
-    }
-    if (game.score() >= 3) {
-        sprite3.set(LedSpriteProperty.X, old_x_2)
-        sprite3.set(LedSpriteProperty.Y, old_y_2)
+    if (x == x_target && y == y_target) {
+        score += 1
     }
     basic.pause(1000)
 }
-game.gameOver()
+basic.showIcon(IconNames.No)
