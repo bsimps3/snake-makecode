fd68efed5e6ecbfe7324b75ac59c48e24d108cd0
Brian Simpson <bsimps3@gmail.com>
2021-10-15 11:35:38 -0500

    added array to track position of snake

--- main.ts
+++ main.ts
@@ -1,7 +1,10 @@
 function targetNewPoint () {
-    while (x_target == x && y_target == y || (score >= 2 && (x_target == old_x && y_target == old_y) || x_target == old_x_2 && y_target == old_y_2)) {
+    for (let index = 0; index <= list.length / 2; index++) {
         x_target = randint(0, 4)
         y_target = randint(0, 4)
+        if (!(x_target == list[index]) && !(y_target == list[index + 1])) {
+            break;
+        }
     }
 }
 input.onButtonPressed(Button.A, function () {
@@ -17,32 +20,22 @@ input.onLogoEvent(TouchButtonEvent.Pressed, function () {
     direction = "up"
 })
 function drawScreen () {
-    led.plot(x, y)
     led.plot(x_target, y_target)
-    if (score >= 2) {
-        led.plot(old_x, old_y)
-    }
-    if (score >= 3) {
-        led.plot(old_x_2, old_y_2)
+    for (let index = 0; index <= score; index++) {
+        led.plot(list[2 * index], list[2 * index + 1])
     }
 }
-let old_y_2 = 0
-let old_x_2 = 0
-let y_target = 0
-let x_target = 0
-let old_y = 0
-let old_x = 0
 let y = 0
 let x = 0
+let y_target = 0
+let x_target = 0
+let list: number[] = []
 let direction = ""
 let score = 0
-score = 1
+score = 0
 direction = "right"
-x = 0
-y = 0
-old_x = 0
-old_y = 0
-led.toggle(x, y)
+list = [0, 0]
+led.toggle(list[0], list[1])
 x_target = randint(0, 4)
 y_target = randint(0, 4)
 led.toggle(x_target, y_target)
@@ -50,22 +43,20 @@ drawScreen()
 basic.pause(1000)
 while (x < 5 && (x >= 0 && (y < 5 && y >= 0))) {
     basic.clearScreen()
-    old_x_2 = old_x
-    old_y_2 = old_y
-    old_x = x
-    old_y = y
+    list.push(list[0])
+    list.push(list[1])
     // movement if statements
     // 
     if (direction == "right") {
-        x += 1
+        list[0] = list[0] + 1
     } else if (direction == "down") {
-        y += 1
+        list[1] = list[1] + 1
     } else if (direction == "left") {
-        x += -1
+        list[0] = list[0] + -1
     } else {
-        y += -1
+        list[1] = list[0] + -1
     }
-    if (x == x_target && y == y_target) {
+    if (list[0] == x_target && list[1] == y_target) {
         score += 1
         targetNewPoint()
         led.plot(x_target, y_target)
@@ -73,7 +64,7 @@ while (x < 5 && (x >= 0 && (y < 5 && y >= 0))) {
     drawScreen()
     for (let index = 0; index < 20; index++) {
         basic.pause(50)
-        led.toggle(x, y)
+        led.toggle(list[0], list[1])
     }
 }
 basic.showIcon(IconNames.No)
