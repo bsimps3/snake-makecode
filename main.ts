a18d6b36f484240bd9c170831d3d22b6e4b772b8
Brian Simpson <bsimps3@gmail.com>
2021-10-15 10:37:42 -0500

    can use going off screen to end game, added clear screen and draw screen

--- main.ts
+++ main.ts
@@ -10,23 +10,40 @@ input.onButtonPressed(Button.B, function () {
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
     direction = "up"
 })
-function move2nd () {
-	
+function drawScreen () {
+    led.plot(x, y)
+    led.plot(x_target, y_target)
+    if (score >= 2) {
+        led.plot(old_x, old_y)
+    }
+    if (score >= 3) {
+        led.plot(old_x_2, old_y_2)
+    }
 }
-let old_y = 0
 let old_y_2 = 0
-let old_x = 0
 let old_x_2 = 0
-let direction = ""
-let score = 1
-direction = "right"
-let x = 0
+let y_target = 0
+let x_target = 0
+let old_y = 0
+let old_x = 0
 let y = 0
+let x = 0
+let direction = ""
+let score = 0
+score = 1
+direction = "right"
+x = 0
+y = 0
+old_x = 0
+old_y = 0
 led.toggle(x, y)
-let x_target = randint(0, 4)
-let y_target = randint(0, 4)
+x_target = randint(0, 4)
+y_target = randint(0, 4)
 led.toggle(x_target, y_target)
+drawScreen()
+basic.pause(1000)
 while (x < 5 && (x >= 0 && (y < 5 && y >= 0))) {
+    basic.clearScreen()
     old_x_2 = old_x
     old_y_2 = old_y
     old_x = x
@@ -34,25 +51,22 @@ while (x < 5 && (x >= 0 && (y < 5 && y >= 0))) {
     // movement if statements
     // 
     if (direction == "right") {
-        led.toggle(x, y)
         x += 1
-        led.toggle(x, y)
     } else if (direction == "down") {
-        led.toggle(x, y)
         y += 1
-        led.toggle(x, y)
     } else if (direction == "left") {
-        led.toggle(x, y)
         x += -1
-        led.toggle(x, y)
     } else {
-        led.toggle(x, y)
         y += -1
-        led.toggle(x, y)
+        if (x == x_target && y == y_target) {
+            led.unplot(x_target, y_target)
+            score += 1
+            x_target = randint(0, 4)
+            y_target = randint(0, 4)
+            led.plot(x_target, y_target)
+        }
     }
-    if (x == x_target && y == y_target) {
-        score += 1
-    }
+    drawScreen()
     basic.pause(1000)
 }
 basic.showIcon(IconNames.No)
