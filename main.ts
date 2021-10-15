b3e146a764b734c23cf955cb7be5d8fd44aca95e
Brian Simpson <bsimps3@gmail.com>
2021-10-14 21:54:54 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,57 @@
+input.onButtonPressed(Button.A, function () {
+    if (sprite.get(LedSpriteProperty.Direction) == 0 || sprite.get(LedSpriteProperty.Direction) == 180) {
+        sprite.set(LedSpriteProperty.Direction, -90)
+    }
+})
+input.onPinPressed(TouchPin.P2, function () {
+    if (sprite.get(LedSpriteProperty.Direction) == -90 || sprite.get(LedSpriteProperty.Direction) == 90) {
+        sprite.set(LedSpriteProperty.Direction, 180)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    if (sprite.get(LedSpriteProperty.Direction) == 0 || sprite.get(LedSpriteProperty.Direction) == 180) {
+        sprite.set(LedSpriteProperty.Direction, 90)
+    }
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    if (sprite.get(LedSpriteProperty.Direction) == -90 || sprite.get(LedSpriteProperty.Direction) == 90) {
+        sprite.set(LedSpriteProperty.Direction, 0)
+    }
+})
+let sprite3: game.LedSprite = null
+let sprite2: game.LedSprite = null
+let old_y = 0
+let old_y_2 = 0
+let old_x = 0
+let old_x_2 = 0
+let sprite: game.LedSprite = null
+game.setScore(1)
+sprite = game.createSprite(1, 1)
+let target = game.createSprite(randint(0, 4), randint(0, 4))
+while (true) {
+    old_x_2 = old_x
+    old_y_2 = old_y
+    old_x = sprite.get(LedSpriteProperty.X)
+    old_y = sprite.get(LedSpriteProperty.Y)
+    sprite.move(1)
+    if (sprite.isTouching(target)) {
+        game.addScore(1)
+        if (game.score() == 2) {
+            sprite2 = game.createSprite(old_x, old_y)
+        }
+        if (game.score() == 3) {
+            old_x_2 = old_x
+            old_y_2 = old_y
+            sprite3 = game.createSprite(old_x_2, old_y_2)
+        }
+    }
+    if (game.score() >= 2) {
+        sprite2.set(LedSpriteProperty.X, old_x)
+        sprite2.set(LedSpriteProperty.Y, old_y)
+    }
+    if (game.score() >= 3) {
+        sprite3.set(LedSpriteProperty.X, old_x_2)
+        sprite3.set(LedSpriteProperty.Y, old_y_2)
+    }
+    basic.pause(1000)
+}
